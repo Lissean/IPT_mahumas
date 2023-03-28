2f3234b05e5abba8c7b5514c73ec2ff0ec23c5ce
Jonavia Mahumas <91597335+Lissean@users.noreply.github.com>
2023-03-28 11:50:49 +0800

    Add files via upload

---
+++ mahumas_act2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$number = 9;
+$result = 0;
+echo "Multiplication Table of $number <br><br>";
+
+for ($i = 1; $i <= 10; $i++) {
+    $result = $i*9;
+    echo "9 x $i = $result <br>";
+}
+
+?>
+
+</body>
+</html>
